1d85cbca8575850e4dae440d658345770b2b1afc
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-10 12:23:32 +0100

    ChooseCard.vue : add img using v-bind and v-for

--- src/components/ChooseCard.vue
+++ src/components/ChooseCard.vue
@@ -2,20 +2,36 @@
 	<div>
 		<div class="wrapper">
 			<div class="info">
-				
+				<p><img v-bind:src="first.imgDog" alt="dogInfoBubble"></p>
 			</div>
 			<div class="cards">
-				
+				<p class="card">
+					<img v-for="card in second.cards" :src="card">
+				</p>
 			</div>
 		</div>
+		<!-- <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>-->
 	</div>
 </template>
 
 <script>
+
 export default {
 	data() {
 		return {
-
+			first: {
+				imgDog : require('../assets/doggycorn.png'),
+			},
+			second: {
+				cards: [
+					require ('../assets/choose-card/beige.svg'),
+					require ('../assets/choose-card/orange.svg'),
+					require ('../assets/choose-card/blue.svg'),
+					require ('../assets/choose-card/red.svg'),
+					require('../assets/choose-card/orange-blue.svg'),
+					require('../assets/choose-card/green.svg'),
+				]
+			}
 		}
 	}
 }
@@ -23,8 +39,46 @@ export default {
 
 <style>
 	.wrapper {
-		height: 100vh;
+		height: 90vh;
 		width: 100%;
-		background: #ccc
+		background: #ccc;
+		display: flex;
+		align-items: flex-end;
+		justify-content: space-around;
 	}
+	.infor {
+		border: 2px solid #000;
+	}
+	.info img{
+		height: 230px;
+		width: 200px;
+	}
+	.cards {
+		border: 2px solid red;
+		justify-content: center;
+		align-items: center;
+	}
+	.card img{
+		width: 100px;
+		height: 100px;
+		margin: 10px;
+		padding: 10px;
+		cursor: pointer;
+	}
+
+@keyframes animation-card {
+	12.5% {transform: rotate(-5deg);}
+	25% {transform: rotate(-10deg);}
+	37.5% {transform: rotate(-5deg);}
+	50% {transform: rotate(0deg);}
+	62.5% {transform: rotate(5deg);}
+	75% {transform: rotate(10deg);}
+	87.5% {transform: rotate(5deg);}
+	100% {transform: rotate(0deg);}
+}
+.card img:hover {
+	animation: animation-card linear 1s infinite;
+}
 </style>
+
+
