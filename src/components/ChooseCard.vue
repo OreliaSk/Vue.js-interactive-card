b2c6f0d6273c6244873af28825392da770e44ab7
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-10 17:26:38 +0100

    ChooseCard.vue : v-if clic card -> new div

--- src/components/ChooseCard.vue
+++ src/components/ChooseCard.vue
@@ -3,13 +3,16 @@
 		<div class="wrapper">
 			<div class="info">
 				<p>
-					<img v-bind:src="first.imgDog" alt="dogInfoBubble">
+					<img v-bind:src="first.imgDog" alt="dogInfoBubble" />
 				</p>
 			</div>
-			<div class="cards">
+			<div v-if="!second.choosen" class="cards">
 				<div class="card">
-					<img v-for="card in second.cards" :src="card" v-on:mouseover="active=!active">
+					<img v-for="card in second.cards" :src="card" v-on:click="changeImage" />
 				</div>
+			</div>
+			<div v-if="second.choosen" class="selection">
+				coucou tout le monde !
 			</div>
 		</div>
 		<!-- <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>-->
@@ -22,7 +25,7 @@ export default {
 	data() {
 		return {
 			first: {
-				imgDog : require('../assets/doggycorn.png'),
+				imgDog : require('../assets/chienunicorn.png'),
 			},
 			second: {
 				cards: [
@@ -32,9 +35,15 @@ export default {
 					require('../assets/choose-card/red.svg'),
 					require('../assets/choose-card/orange-blue.svg'),
 					require('../assets/choose-card/green.svg'),
-				]
+				],
+				bubble: require('../assets/choose-card/speech-bubble.svg'),
+				choosen: false,
 			},
-			active:false,
+		}
+	},
+	methods: {
+		changeImage:function() {
+			this.second.choosen = true;
 		}
 	}
 }
@@ -44,31 +53,28 @@ export default {
 	.wrapper {
 		height: 90vh;
 		width: 100%;
-		background: #ccc;
+		background: #c9ddff;
 		display: flex;
 		align-items: center;
 		justify-content: space-around;
 	}
 	.info {
-		border: 2px solid #000;
 		align-self: flex-end;
 	}
 	.info img{
-		height: 230px;
-		width: 200px;
+		height: 400px;
+		width: 430px;
 	}
 	.cards {
-		border: 2px solid red;
 		justify-content: center;
 		align-self: center;
+		width: 50vw;
 	}
 	.card img{
-		width: 100px;
-		height: 100px;
-		margin: 10px;
-		padding: 10px;
+		width: 150px;
+		height: 150px;
+		margin: 30px;
 		cursor: pointer;
-		border: 2px solid black;
 	}
 
 @keyframes animation-card {
