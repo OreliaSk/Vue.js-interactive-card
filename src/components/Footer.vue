32422919f8e2c527ea24298c1e75269fcbd82e05
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-10 23:12:15 +0100

    creation component Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+	<footer class="footer">
+		<p class="copyright">
+			Copyright © <a href="">Orélia Sokambi</a> 2018 – Tous droits réservés. Toute reproduction est interdite sans l'autorisation de l'auteur.
+		</p>
+		<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+	</footer>
+</template>
+
+<style scoped>
+	.footer {
+		background-color: crimson;
+		padding: 15px;
+		text-align: center;
+	}
+</style>
