a13d6e05f3be46ce54005c4d9523b6c8cc0ce41e
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-11 23:31:56 +0100

    Footer.vue : reorganization css + background color in blue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,15 +3,22 @@
 		<p class="copyright">
 			Copyright © <a href="https://github.com/OreliaSk/Vue.js-interactive-card">Orélia Sokambi</a> 2018 – Tous droits réservés. Toute reproduction est interdite sans l'autorisation de l'auteur.
 		</p>
-		<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+		<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> and <a href="https://www.flaticon.com/authors/dimitry-miroliubov" title="Dimitry Miroliubov">Dimitry Miroliubov</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 	</footer>
 </template>
 
 <style scoped>
 	.footer {
-		background-color: crimson;
-		padding: 15px;
+		background-color: #1758c1;
+		font-family: 'Montserrat', sans-serif;
+		font-size: 0.65rem;
+		margin: 0;
+		padding: 10px;
 		text-align: center;
+	}
+	.footer a {
+		color: 000;
+		text-decoration: none;
 	}
 </style>
 
