0059bf9144d9afc1356eb1d29b6b72f3133eb69b
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-11 17:18:18 +0100

    Footer.vue : add link for personal landing page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="footer">
 		<p class="copyright">
-			Copyright © <a href="">Orélia Sokambi</a> 2018 – Tous droits réservés. Toute reproduction est interdite sans l'autorisation de l'auteur.
+			Copyright © <a href="https://github.com/OreliaSk/Vue.js-interactive-card">Orélia Sokambi</a> 2018 – Tous droits réservés. Toute reproduction est interdite sans l'autorisation de l'auteur.
 		</p>
 		<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 	</footer>
@@ -14,3 +14,4 @@
 		text-align: center;
 	}
 </style>
+
