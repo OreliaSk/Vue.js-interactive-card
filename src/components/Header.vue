1c5dfeac2c909075706d7927cb17719bb6e9f3ee
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-13 14:18:10 +0100

    Header.vue : add filter (upperCase on title) + css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
 	<nav>
-		<h2>{{ title }}</h2>
+		<h2>{{ title | upperCase }}</h2>
+		<h3>{{ subtitle }}</h3>
 	</nav>
 </template>
 
@@ -9,24 +10,37 @@
 export default {
 	data(){
 		return{
-			title : 'Carte jolie',
+			title : 'des cartes jolies',
+			subtitle : 'Envoyez une carte faite par vos soins à vos proches, pour un événement exceptionnel !',
 		}
 	}, 
+	filters: {
+		'upperCase': function(value){
+			return value.toUpperCase();
+		}
+	}
 }
 </script>
 
 
 <style scoped>
 	nav {
-		background-color: #1758c1;
+		background-color: #89b6d5;
 		margin: 0;
 		text-align: center;
 	}
-	h2 {
+	h2, h3 {
 		color: #fff;
-		font-family: 'Montserrat', sans-serif;
-		letter-spacing: 2px;
+		font-family: 'Lato', sans-serif;
 		margin: 0;
-		padding: 20px;	
+	}
+	h2 {
+		letter-spacing: 0.05px;
+		padding-top: 15px;
+	}
+	h3 {
+		letter-spacing: 0.03px;
+		padding-bottom: 15px;
+		font-weight: normal;
 	}
 </style>
