b2cb4d87a8207092111ce2324049e022fd53b797
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-13 14:42:26 +0100

    Footer.vue : css work on colors and background-color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,23 @@
 <template>
 	<footer class="footer">
 		<p class="copyright">
-			Copyright © <a href="https://github.com/OreliaSk/Vue.js-interactive-card">Orélia Sokambi</a> 2018 – Tous droits réservés. Toute reproduction est interdite sans l'autorisation de l'auteur.
+			Copyright © <a href="https://oreliask.github.io/MDBootstrap-Landing-page/index.html">Orélia Sokambi</a> 2018 – Tous droits réservés. Toute reproduction est interdite sans l'autorisation de l'auteur.
 		</p>
-		<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> and <a href="https://www.flaticon.com/authors/dimitry-miroliubov" title="Dimitry Miroliubov">Dimitry Miroliubov</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+		<p class="icon-ref">Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> and <a href="https://www.flaticon.com/authors/dimitry-miroliubov" title="Dimitry Miroliubov">Dimitry Miroliubov</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
 	</footer>
 </template>
 
 <style scoped>
 	.footer {
-		background-color: #1758c1;
-		font-family: 'Montserrat', sans-serif;
+		background-color: #89b6d5;
+		font-family: 'Lato', sans-serif;
 		font-size: 0.65rem;
 		margin: 0;
 		padding: 10px;
 		text-align: center;
 	}
-	.footer a {
-		color: 000;
+	a{
+		color: #c4e6ff;
 		text-decoration: none;
 	}
 </style>
